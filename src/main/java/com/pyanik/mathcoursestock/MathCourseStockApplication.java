9104d5932130cb90ffedd3a989634527ef435c0e
pyanik <pyanik@users.noreply.github.com>
2022-10-09 12:00:12 +0200

    initial commit

---
+++ src/main/java/com/pyanik/mathcoursestock/MathCourseStockApplication.java
@@ -0,0 +1,13 @@
+package com.pyanik.mathcoursestock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathCourseStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathCourseStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pyanik/mathcoursestock/MathCourseStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pyanik.mathcoursestock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathCourseStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
