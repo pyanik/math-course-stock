066d0bddb4bbe530b5778dcd4a8956406988d578
pyanik <pyanik@users.noreply.github.com>
2022-10-10 21:41:58 +0200

    add instructor persistence layer

---
+++ src/main/java/com/pyanik/mathcoursestock/instructor/persistence/InstructorRepository.java
@@ -0,0 +1,8 @@
+package com.pyanik.mathcoursestock.instructor.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InstructorRepository extends JpaRepository<Instructor, Long> {
+}
