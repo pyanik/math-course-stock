090c4ca13e7d32ab66300cd53c6462eeff414932
pyanik <pyanik@users.noreply.github.com>
2022-10-12 20:42:33 +0200

    correct the typos

--- src/main/java/com/pyanik/mathcoursestock/instructor/persistence/InputInstructorDTO.java
+++ src/main/java/com/pyanik/mathcoursestock/instructor/persistence/InputInstructorDTO.java
@@ -5,12 +5,12 @@ import javax.validation.constraints.*;
 public record InputInstructorDTO(@NotBlank(message = "first name must be present")
                                  @NotEmpty(message = "first name must not be empty")
                                  @Size(max = 20, message = "first name must not be longer than 20 letters")
-                                 @Pattern(regexp = "[a-z A-Z]+", message = "first name must contains only letter") String firstName,
+                                 @Pattern(regexp = "[a-z A-Z]+", message = "first name must contains only letters") String firstName,
 
                                  @NotBlank(message = "last name must be present")
                                  @NotEmpty(message = "last name must not be empty")
                                  @Size(max = 20, message = "last name must not be longer than 20 letters")
-                                 @Pattern(regexp = "[a-z A-Z]+", message = "last name must contains only letter") String lastName,
+                                 @Pattern(regexp = "[a-z A-Z]+", message = "last name must contains only letters") String lastName,
 
                                  @Email(message = "email should be a valid email") String email,
 
